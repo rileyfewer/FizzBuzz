9c1aacf030f06a3f614524bff4a386522fbde80b
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-09-15 13:37:05 -0400

    reduce is working

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Reduce {
-}
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int count = 0;
+        while (i > 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+                count += 1;
+            }
+            if (i % 2 != 0) {
+                i = i - 1;
+                count += 1;
+            }
+        }
+        System.out.println(count - 1);
+        System.out.println(i);
+    }}
