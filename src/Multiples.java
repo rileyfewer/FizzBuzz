a5946ebdb4f8f7bcd614a12da58a8d96d5b81dcd
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-09-15 12:07:54 -0400

    Multiples finished and working - pushing changes

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,11 +1,9 @@
 public class Multiples {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 1;
         while (i < 100) {
             boolean divisibleBy3 = i % 3 == 0;
-            i++;
             boolean divisibleBy5 = i % 5 == 0;
-            i++;
 
             if (divisibleBy3 && divisibleBy5) {
                 System.out.println(i);
@@ -15,9 +13,8 @@ public class Multiples {
             }
             if (divisibleBy3) {
                 System.out.println(i);
-            } else {
-                System.out.println(i);
             }
+            i++;
         }
     }
 }
